062d8275df712fd1cc8bc334de541df6aec16fdb
James Harold II <JHarold1241@outlook.com>
2024-09-16 21:23:57 -0500

    Initial concept for the amber framework boilerplate

---
+++ .history/db/migrations/20240917020751498_create_user_20240916211109.sql
@@ -0,0 +1,15 @@
+-- +micrate Up
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR, 
+  username VARCHAR,
+  email VARCHAR,
+  hashed_password VARCHAR,
+  confirm_password VAR
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP
+);
+
+
+-- +micrate Down
+DROP TABLE IF EXISTS users;
---
+++ src/assets/javascripts/main.js
@@ -0,0 +1,2 @@
+import 'bootstrap';
+import Logo from '../images/logo.svg';
